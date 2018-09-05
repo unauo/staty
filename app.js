d8687522b66ad00671570807030d3bf52c3831ec
unauo <asliumay@gmail.com>
2018-09-05 17:50:15 +0300

    rearrange folders

--- app.js
+++ app.js
@@ -6,19 +6,19 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
 	//	res.send('this is the homepage');
-	res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/htmls/index.html');
 });
 app.get('/stats', function(req, res){
 	//	res.send('this is the homepage');
-	res.sendFile(__dirname + '/stats.html');
+	res.sendFile(__dirname + '/htmls/stats.html');
 });
 app.get('/about', function(req, res){
 	//	res.send('this is the homepage');
-	res.sendFile(__dirname + '/about.html');
+	res.sendFile(__dirname + '/htmls/about.html');
 });
 app.get('/feedback', function(req, res){
 	//	res.send('this is the homepage');
-	res.sendFile(__dirname + '/feedback.html');
+	res.sendFile(__dirname + '/htmls/feedback.html');
 });
 app.use(express.static('public')); 
 
