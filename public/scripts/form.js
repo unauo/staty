61abd6c2dd2987d78da9d03bfcfcbdd03d28f9c0
unauo <asliumay@gmail.com>
2018-09-13 14:41:45 +0300

    add main info on individual form page

---
+++ public/scripts/form.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const allForms = JSON.parse(window.localStorage.getItem('paymentForms'));
+var header = document.querySelector("#form-info");
+var formInfo;
+
+function setForm(formID) {
+	for (let i = 0; i < allForms.length; i++) {
+		if (allForms[i].id == formID) {
+			formInfo= allForms[i];
+			return formInfo;
+		}
+	}
+	return false;
+}
+
+function editHeader() {
+	var title = document.createElement('h2');
+	title.innerHTML = `Statistics for your form: <i>"${formInfo.name}"</i>`;
+	header.appendChild(title);
+	var total = document.createElement('p');
+	total.innerHTML = `Total earnings from this form: ${formInfo.total.toFixed(2)} ${formInfo.currency}`;
+	header.appendChild(total);
+	var status = document.createElement('p');
+	status.innerHTML = `${ formInfo.status == 'ENABLED' ? 'This form is still accepting submissions.' : 'This form is not accepting submissions.'}`
+	header.appendChild(status);
+	var sublink = document.createElement('p');
+	sublink.innerHTML = `<a href = "https://jotform.com/submissions/${formInfo.id}">Click here</a> to view this form's submissions on JotForm.`;
+	header.appendChild(sublink);
+}
+
+function getFormQuestions(formID, apiKey) {
+	axios.get('https://api.jotform.com/form/' + formID + '/questions?apiKey=' + apiKey)
+		.then(function (response) {
+    		console.log(response);
+  		})
+  		.catch(function (error) {
+    		console.log(error);
+  		})
+  		.then(function () {
+  			console.log('hello');
+  		});
+}
+
--- public/scripts/stats.js
+++ public/scripts/stats.js
@@ -1,38 +1,38 @@
-"use strict";
+'use strict';
 
 var apiKey;
 var types = [
-"control_square",
-"control_paypal",
-"control_wepay",
-"control_authnet",
-"control_echeck",
-"control_stripe",
-"control_stripeACH",
-"control_sofort",
-"control_moneris",
-"control_payu",
-"control_pagseguro",
-"control_bluesnap",
-"control_paymentwall",
-"control_payment",
-"control_paypalexpress",
-"control_paypalpro",
-"control_payjunction",
-"control_chargify",
-"control_bluepay",
-"control_braintree",
-"control_2co",
-"control_cardconnect",
-"control_worldpay",
-"control_worldpayus",
-"control_eway",
-"control_firstdata",
-"control_paysafe",
-"control_skrill",
-"control_gocardless",
-"control_clickbank",
-"control_onebip"
+'control_square',
+'control_paypal',
+'control_wepay',
+'control_authnet',
+'control_echeck',
+'control_stripe',
+'control_stripeACH',
+'control_sofort',
+'control_moneris',
+'control_payu',
+'control_pagseguro',
+'control_bluesnap',
+'control_paymentwall',
+'control_payment',
+'control_paypalexpress',
+'control_paypalpro',
+'control_payjunction',
+'control_chargify',
+'control_bluepay',
+'control_braintree',
+'control_2co',
+'control_cardconnect',
+'control_worldpay',
+'control_worldpayus',
+'control_eway',
+'control_firstdata',
+'control_paysafe',
+'control_skrill',
+'control_gocardless',
+'control_clickbank',
+'control_onebip'
 ];
 var rawForms = [];
 var forms = [];
@@ -49,11 +49,11 @@ var width = 700,
 var x = d3.scaleLinear()
     .range([1, 400]);
 
-var chart = d3.select("#bar-chart")
-    .attr("width", width);
+var chart = d3.select('#bar-chart')
+    .attr('width', width);
 
 function onError() {
-	alert("Oops, something went wrong, or not.");
+	alert('Oops, something went wrong, or not.');
 }
 
 function arrayify(rawForms, forms){
@@ -63,8 +63,8 @@ function arrayify(rawForms, forms){
 // we need the index of the first unshown form with the given total earnings. this will be crucial if there are several forms with the same total.
 function findForm(total) {
 	for (let i = 0; i < chartData.length; i ++) {
-		if ((chartData[i].status == 0) && (parseFloat(chartData[i].total) == total)) {
-			chartData[i].status = 1;
+		if ((chartData[i].isShown == 0) && (parseFloat(chartData[i].total) == total)) {
+			chartData[i].isShown = 1;
 			return i;
 		}
 	}
@@ -73,41 +73,41 @@ function findForm(total) {
 function updateGraph() {
 	ttlsData = ttlsData.sort(function(a,b) { return b-a; });
 	x.domain([0, d3.max(ttlsData)]);
-	chart.attr("height", barHeight * ttlsData.length + 20);
+	chart.attr('height', barHeight * ttlsData.length + 20);
 	
-	var bar = chart.selectAll("g")
+	var bar = chart.selectAll('g')
     	.data(ttlsData)
-    .enter().append("g")
-    	.attr("transform", function(d, i) { return "translate(5," + (i * barHeight + 5) + ")"; });
+    .enter().append('g')
+    	.attr('transform', function(d, i) { return 'translate(5,' + (i * barHeight + 5) + ')'; });
 
-  	bar.append("rect")
-    	.attr("width", x)
-    	.attr("height", barHeight - 4);
+  	bar.append('rect')
+    	.attr('width', x)
+    	.attr('height', barHeight - 4);
 
-  	bar.append("text")
-      	.attr("x", function(d) { return x(d)+3; })
-      	.attr("y", barHeight / 2)
-      	.attr("dy", ".20em")
+  	bar.append('text')
+      	.attr('x', function(d) { return x(d)+3; })
+      	.attr('y', barHeight / 2)
+      	.attr('dy', '.20em')
 		.text( function(d) { 
 			let i = findForm(d);
-			return chartData[i].name + " (" + (chartData[i].total).toFixed(2) + " " + chartData[i].currency + ")"; 
+			return chartData[i].name + ' (' + (chartData[i].total).toFixed(2) + ' ' + chartData[i].currency + ')'; 
 		});
 
-	chart.append("g")
-      	.attr("class", "axis axis--x")
-      	.attr("transform", "translate( 5," + (ttlsData.length * barHeight + 2) + ")")
+	chart.append('g')
+      	.attr('class', 'axis axis-x')
+      	.attr('transform', 'translate( 5,' + (ttlsData.length * barHeight + 2) + ')')
       	.call(d3.axisBottom(x).tickFormat(function(d){ return d;}));
 
-
+    window.localStorage.setItem('paymentForms', JSON.stringify(chartData));
 }
 
 function addFormToTable(form, total, currency){
 	// we need 2 seperate arrays for data. first array only consists of the total earnings, so that we can sort our data.
-	// second array has the info to be shown for each form. status is -1 if the form is not shown yet. more detail can be found in "findForm(total)".
+	// second array has the info to be shown for each form. isShown is 0 if the form is not shown yet. more detail can be found in 'findForm(total)'.
 	table.innerHTML +=
 			`<tr>
-			<td scope="row"><a href="/form/${form[0]}">${form[1]}</a></td> 
-			<td>${form[2] == "ENABLED" ? "yes" : "no"}</td>
+			<td scope='row'><a href='/form/${form[0]}'>${form[1]}</a></td> 
+			<td>${form[2] == 'ENABLED' ? 'yes' : 'no'}</td>
 			<td>${total} ${currency}</td>
 			</tr>`;
 	setTimeout(() => {updateGraph()}, 300);
@@ -123,13 +123,13 @@ function getTotalEarned(form, qid){
 			}
 		}
 		ttlsData.push(grandTotal);
-		chartData.push({status: 0, name: form[1], total: grandTotal, currency: currency});
+		chartData.push({isShown: 0, name: form[1], total: grandTotal, currency: currency, status: form[2], question: qid, id: form[0]});
 		addFormToTable(form, grandTotal.toFixed(2), currency)
 	}, onError);
 }
 
 function isPayment(form) {
-	if (form[2] == "DELETED") return false;
+	if (form[2] == 'DELETED') return false;
 	let questions = [];
 	JF.getFormQuestions(form[0],function(res) {
 		questions = res;
@@ -145,10 +145,10 @@ function isPayment(form) {
 
 
 
-window.addEventListener("load", function(){	
+window.addEventListener('load', function(){	
 	// initialize API key
-	apiKey = localStorage.getItem('apiKey');
-	if (!apiKey) window.alert("You are not logged in, please navigate to home for logging in first.");
+	apiKey = window.localStorage.getItem('apiKey');
+	if (!apiKey) window.alert('You are not logged in, please navigate to home for logging in first.');
 	JF.initialize({apiKey:apiKey});
 	// get forms and check if they are payment forms
 	JF.getForms(function(res){rawForms = res;
